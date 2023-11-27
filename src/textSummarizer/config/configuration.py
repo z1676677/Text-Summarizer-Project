a2782c17619d8a80c40f4b1a14d127b3613eebf1
zy <z16766773@gmail.com>
2023-11-28 06:22:59 +0800

    project completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -81,7 +81,7 @@ class ConfigurationManager:
 
         return model_trainer_config
 
-def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config = self.config.model_evaluation
 
         create_directories([config.root_dir])
